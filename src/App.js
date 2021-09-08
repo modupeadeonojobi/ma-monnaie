70455f188c589931acda62140ba785a0ac31622a
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-09-08 12:39:21 +0100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
   const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch);
 
   return (
-    <div className="App">
-      <h1>{account}</h1>
-      <button onClick={() => depositMoney(1000)}> Deposit </button>
-      <button onClick={() => withdrawMoney(100)}> Withdraw </button>
+    <div className="container">
+      <h1 className="m-5 align-self-center">{account}</h1>
+      <button className="btn btn-primary btn-sm" onClick={() => depositMoney(1000)}> Deposit </button>
+      <button className="btn btn-primary btn-sm m-2" onClick={() => withdrawMoney(100)}> Withdraw </button>
     </div>
   );
 }
